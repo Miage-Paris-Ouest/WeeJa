bac1b5d215eaa5e0c81fa833b4857633e4adc625
AlexandreCARNEADO <acarneado@yahoo.fr>
2016-11-09 14:56:50 +0100

    commit

---
+++ app/src/main/java/projet/holo_holo/weeja/controller_view/RegisterActivity.java
@@ -0,0 +1,29 @@
+package projet.holo_holo.weeja.controller_view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import projet.holo_holo.weeja.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button boutonRegister = (Button) findViewById(R.id.boutonRegister);
+
+        boutonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
