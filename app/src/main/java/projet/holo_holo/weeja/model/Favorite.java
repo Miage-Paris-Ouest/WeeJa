e4dc1765c0612a9c491f36232f13831e22bd9efb
AlexandreCARNEADO <acarneado@yahoo.fr>
2016-11-15 18:25:42 +0100

    ClassDiagram

--- app/src/main/java/projet/holo_holo/weeja/model/Favorite.java
+++ app/src/main/java/projet/holo_holo/weeja/model/Favorite.java
@@ -6,4 +6,30 @@ package projet.holo_holo.weeja.model;
 
 public class Favorite {
 
+    private String type;
+    private String name;
+
+    public Favorite() {
+    }
+
+    public Favorite(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getname() {
+        return name;
+    }
 }
