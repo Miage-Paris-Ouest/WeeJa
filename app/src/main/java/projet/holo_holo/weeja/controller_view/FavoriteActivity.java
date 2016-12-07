1d7f821fff34b70835bb0fb251a16d59b2849d85
AlexandreCARNEADO <acarneado@yahoo.fr>
2016-12-07 16:37:39 +0100

    Affichage des favoris

--- app/src/main/java/projet/holo_holo/weeja/controller_view/FavoriteActivity.java
+++ app/src/main/java/projet/holo_holo/weeja/controller_view/FavoriteActivity.java
@@ -2,6 +2,11 @@ package projet.holo_holo.weeja.controller_view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import projet.holo_holo.weeja.R;
 
@@ -11,5 +16,40 @@ public class FavoriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+        ListView listeHistorique = (ListView) findViewById(R.id.listViewFavorite);
+
+        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
+
+        HashMap<String, String> map;
+
+        map = new HashMap<String, String>();
+
+        map.put("Date", "01/12/2016");
+        map.put("Destination", "à  Boston");
+        map.put("Image", String.valueOf(R.drawable.favoriok));
+
+        listItem.add(map);
+
+        map = new HashMap<String, String>();
+
+        map.put("Date", "23/09/2016");
+        map.put("Destination", "à  New York");
+        map.put("Image", String.valueOf(R.drawable.favoriok));
+
+        listItem.add(map);
+
+        map = new HashMap<String, String>();
+
+        map.put("Date", "23/12/2015");
+        map.put("Destination", "à  Helsinki");
+        map.put("Image", String.valueOf(R.drawable.favoriok));
+
+        listItem.add(map);
+
+        SimpleAdapter mSchedule = new SimpleAdapter(this.getBaseContext(), listItem, R.layout.favorite_rows,
+                new String[] {"Date", "Destination", "Image"}, new int[] {R.id.textViewDateFavorite, R.id.textViewDestinationFavorite, R.id.imageViewFavorite});
+
+
+        listeHistorique.setAdapter(mSchedule);
     }
 }
--- app/src/main/java/projet/holo_holo/weeja/controller_view/HistoryActivity.java
+++ app/src/main/java/projet/holo_holo/weeja/controller_view/HistoryActivity.java
@@ -14,8 +14,6 @@ import projet.holo_holo.weeja.R;
 
 public class HistoryActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
